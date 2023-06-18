875d8bbf00a751d54dd35b103f0baaa1eeca702e
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-06-18 21:17:19 +0300

    ask for help on this one

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    let newArray {
+    for (let i = 0; i< name.length; i++) {
+        console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }return newArray
+}
+}
+writeCards([], "")
+
